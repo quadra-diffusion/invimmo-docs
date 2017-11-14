44982401c59d7d0e3a8f3aa793d6b67440755ef1
Quadra-Diffusion <administratif@quadra-diffusion.fr>
2017-11-14 15:25:04 +0100

    Correction espaces

--- createBadExamples.sh
+++ createBadExamples.sh
@@ -12,6 +12,6 @@ sed -e 's/"ReferenceExterneLocataire",/"ReferenceExterneLocataire","xx" : "yy",/
 sed -e 's/"ReferenceExterneCompteur",/"ReferenceExterneCompteur","xx" : "yy",/' exemples/patrimoine-complet.json > bad/patrimoine-champ-additionnel-compt.json
 find bad/patrimoine* -maxdepth 1 -type f -print0 | xargs -0 -I {} ajv test -s patrimoine.schema.json -d {} --invalid
 
-sed -e 's/^\s"ReferenceExterneBail": "ReferenceExterneBail",//' exemples/planification-complet.json > bad/planification-manque-reference-bail.json
-sed -e 's/^\s"Date": "2017-11-14T10:44:14+01:00",/"Date": "2017-11-14",/' exemples/planification-complet.json > bad/planification-date-et-heure-obligatoire.json
+sed -e 's/"ReferenceExterneBail": "ReferenceExterneBail",//' exemples/planification-complet.json > bad/planification-manque-reference-bail.json
+sed -e 's/"Date": "2017-11-14T10:44:14+01:00",/"Date": "2017-11-14",/' exemples/planification-complet.json > bad/planification-date-et-heure-obligatoire.json
 find bad/planification* -maxdepth 1 -type f -print0 | xargs -0 -I {} ajv test -s planification.schema.json -d {} --invalid
