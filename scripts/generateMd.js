731614cd8cf3d000cf7f7b61c3e8bc556468b303
Quadra-Diffusion <administratif@quadra-diffusion.fr>
2017-11-14 15:52:46 +0100

    Gestion de la doc de la planification

--- scripts/generateMd.js
+++ scripts/generateMd.js
@@ -1,6 +1,7 @@
 var Parser = require('jsonschema-markdown');
 var fs = require('fs');
 var schema = require('../patrimoine.schema.json');
+var schemaPlanification = require('../planification.schema.json');
 
 var generator = new Parser(schema, { });
 generator.addSchema(schema.items);
@@ -9,14 +10,25 @@ generator.addSchema(schema.items.properties.Baux.items.properties.Compteurs.item
 generator.addSchema(schema.items.properties.Baux.items.properties.Locataires.items);
 generator.addSchema(schema.items.properties.Proprietaire);
 
+var generatorPlanification = new Parser(schemaPlanification, { });
+generatorPlanification.addSchema(schemaPlanification.items);
+
 var markdown = generator.generateMarkdown();
+var markdownPlanification = generatorPlanification.generateMarkdown();
 
 fs.writeFile("connecteur_patrimoine.md", markdown, function(err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log("The md file was saved!");
+    console.log("The pat md file was saved!");
+});
+fs.writeFile("connecteur_planification.md", markdownPlanification, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The plan md file was saved!");
 });
 
 
